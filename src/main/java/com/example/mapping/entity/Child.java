d8a08a5cf42427fc0bbfbe06c86fd39fb4ba0c19
Devamatha <p.devamatha2001@gmail.com>
2024-04-02 18:30:51 +0530

    Many to one

---
+++ src/main/java/com/example/mapping/entity/Child.java
@@ -0,0 +1,28 @@
+package com.example.mapping.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Child {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long childId;
+	private String childName;
+	private String villageName;
+	private String surName;
+
+	@ManyToOne
+	private Parent parent;
+
+}
